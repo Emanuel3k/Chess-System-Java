33097211aa7c5ba06adf0ea9de31d637aaaec7af
Emanuel Santos <Emanuelsantos_677@hotmail.com>
2023-03-21 00:26:27 -0300

    Moving Pieces

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,13 +1,29 @@
 package app;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
-        Ui.PrintBoard(chessMatch.getPieces());
+         while (true) {
+            Ui.PrintBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.println("Source: ");
+            ChessPosition source = Ui.readChessPosition(sc);
+
+            System.out.println();
+            System.out.println("Target: ");
+            ChessPosition target = Ui.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performeChessMove(source, target);
+         }
 
     }
 }
